586dab4560ca25b4e4c66eb37b9142a933fd5f84
codekongs <531173160@qq.com>
2017-10-31 13:44:59 +0800

    Python使用第三方包

---
+++ day5/PillowDdemo.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import sys
+
+from PIL import Image
+im = Image.open('test.png')
+print(im.format, im.size, im.mode)
+im.thumbnail((200, 100))
+im.save('thumb.jpg', 'JPEG')
+
+print(sys.path)
---
+++ day5/test.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import module
+module.test()
+print(module.__doc__)
+print(module.__author__)
+print(module.__file__)
+print(module.__name__)
+print(module.__package__)
