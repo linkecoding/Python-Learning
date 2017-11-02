03695a9bcef62775a6e6f562bc2e332de2999a46
codekongs <531173160@qq.com>
2017-11-02 20:04:53 +0800

    python面向对象

---
+++ day6/oop.py
@@ -0,0 +1,42 @@
+class Student(object):
+    """docstring for Student"""
+
+    def __init__(self, name, age):
+        super(Student, self).__init__()
+        self.__name = name
+        self.__age = age
+
+
+class Animal(object):
+    """docstring for Animal"""
+
+    def run(self):
+        print('Animal is running')
+
+
+class Dog(Animal):
+    """docstring for Dog"""
+
+    def run(self):
+        print('Dog is running')
+
+
+class Cat(Animal):
+    """docstring for Cat"""
+
+    def run(self):
+        print('Cat is running')
+
+
+def test(animal):
+    animal.run()
+
+if __name__ == '__main__':
+    animal = Animal()
+    cat = Cat()
+    dog = Dog()
+    test(animal)
+    test(cat)
+    test(dog)
+
+    student = Student()
